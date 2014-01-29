9bae126f6cd7faa18a27e1c8cbbebd23fee4ded6
MatthewTLackey <lackey.matt@gmail.com>
2014-01-29 16:16:23 -0500

    Fills in layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,25 +5,24 @@ describe "Static pages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
+    before {visit root_path }
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
 
     it "should not have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
-    it "should not have '| Home'" do
-       visit '/static_pages/home'
+    it "should not have a custom page title" do
        expect(page).not_to have_title('| Home')
     end
   end
 
   describe "Help page" do
-
+    before { visit '/static_pages/help'}
+    
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')

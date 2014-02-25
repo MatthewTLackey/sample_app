83f307bb39b556f0e5bae1209949d29c8ee03575
MatthewTLackey <lackey.matt@gmail.com>
2014-02-25 10:53:23 -0500

    Adds javascript counter for micropoost

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
+//= require micropost.js
 //= require_tree .
---
+++ app/assets/javascripts/micropost.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("#micropost_content").keyup(function() {
+    var count = 140 - $(this).val().length;
+    if(count >= 0){
+      if(count === 1){
+        $("#counter").text(count + " character remaining")
+      } else {
+        $("#counter").text(count + " characters remaining")
+      }
+      $("#counter").css("color", "black");
+    } else if (count < 0) {
+      if(count === -1){
+        $("#counter").text(count * -1 + " character over the limit")
+      } else {
+      $("#counter").text(count * -1 + " characters over the limit")
+      }
+      $("#counter").css("color", "red");
+    }
+  });
+});
